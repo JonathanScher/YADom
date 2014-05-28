5fc4c962f97362c4fd4b57e3b2f657a7bedc1faf
Jonathan Scher <scher.jonathan@gmail.com>
2014-05-28 12:22:22 +1200

    Cleanup for GameDeck (using || and && to get rid of if conditions)

--- Dominion/src/dominion/GameDeck.java
+++ Dominion/src/dominion/GameDeck.java
@@ -9,25 +9,19 @@ public class GameDeck extends HashMap<Card, Integer> {
 	private static final long serialVersionUID = 7699189497179269801L;
 
 	public Boolean gameOver(Integer numberOfPlayers) {
-		//TODO might want to add a condition on time or number of turns
+		// TODO might want to add a condition on time or number of turns
 		return provincesAreGone() || tooManyPilesEmptied(numberOfPlayers);
 	}
 
 	private Boolean tooManyPilesEmptied(Integer numberOfPlayers) {
-		Boolean gameOver;
 		Integer numberOfPilesToBeDepleted = NUMBER_OF_PILES_TO_BE_DEPLETED_2_PLAYERS;
 		if (numberOfPlayers > 2) {
 			numberOfPilesToBeDepleted = NUMBER_OF_PILES_TO_BE_DEPLETED_MORE_PLAYERS;
 		}
-		gameOver = Collections.frequency(values(), new Integer(0)) >= numberOfPilesToBeDepleted;
-		return gameOver;
+		return Collections.frequency(values(), new Integer(0)) >= numberOfPilesToBeDepleted;
 	}
 
 	private Boolean provincesAreGone() {
-		Boolean gameOver = false;
-		if (get(Card.PROVINCE) != null) {
-			gameOver = get(Card.PROVINCE) == 0;
-		}
-		return gameOver;
+		return get(Card.PROVINCE) != null && get(Card.PROVINCE) == 0;
 	}
 }
